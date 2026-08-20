000663a415d23509cf839471b86cbdf48424d70d
kipyegonk <kevinlangat008@gmail.com>
2026-08-20 15:46:35 +0300

    Create the web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoiceController;
 
+Route::get('/invoices', [InvoiceController::class, 'indexView']);
+Route::get('/invoices/{id}', [InvoiceController::class, 'showView']);
 Route::get('/', function () {
     return view('welcome');
 });

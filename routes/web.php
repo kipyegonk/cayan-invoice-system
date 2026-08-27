20056d2cb422c5ef6e97b2021a49d0d5748ce616
kipyegonk <kevinlangat008@gmail.com>
2026-08-27 16:45:01 +0300

    Add web route for invoice PDF download

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\InvoiceController;
 
 Route::get('/invoices', [InvoiceController::class, 'indexView']);
 Route::get('/invoices/{id}', [InvoiceController::class, 'showView']);
+Route::get('/invoices/{id}/pdf', [InvoiceController::class, 'pdf']);
 Route::get('/', function () {
     return view('welcome');
 });
